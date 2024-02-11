c7f88e89281389c6231464b8ee9da05ec4a251ca
Vanessa Rice <vanessa.rice@keyin.com>
2024-02-11 12:03:29 -0330

    updated file structure, and css for html files.

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 const { logEvent } = require('./logevents');
 const { handleRoutes } = require('./routes');
 
@@ -11,11 +13,25 @@ server.on('request', (request, response) => {
     // Log the route access
     logEvent(url);
 
-    // Handle routes
-    handleRoutes(url, (statusCode, contentType, data) => {
-        response.writeHead(statusCode, { 'Content-Type': contentType });
-        response.end(data);
-    });
+    // Check if the request is for a static file
+    if (url.startsWith('/public/')) {
+        const filePath = path.join(__dirname, url);
+        fs.readFile(filePath, (err, data) => {
+            if (err) {
+                response.writeHead(404);
+                response.end('Not found');
+            } else {
+                response.writeHead(200);
+                response.end(data);
+            }
+        });
+    } else {
+        // Handle routes
+        handleRoutes(url, (statusCode, contentType, data) => {
+            response.writeHead(statusCode, { 'Content-Type': contentType });
+            response.end(data);
+        });
+    }
 });
 
 // Listen on the correct port
